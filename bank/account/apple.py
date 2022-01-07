490203aeb49cf3bbdd25fecccd462c764a2e862d
5top1t <jarryngandjui@gmail.com>
2022-01-07 10:04:56 -0500

    apple card payments

--- bank/account/apple.py
+++ bank/account/apple.py
@@ -6,7 +6,7 @@ from .factory import AccountFactory
 
 account = "Apple Card"
 arg = "apple"
-payment_payees = ["Online payment from CHK"]
+payment_payees = ["Online payment from CHK", "ACH DEPOSIT"]
 interest_charges = []
 rename_columns = {
     "Transaction Date": "posted date",
